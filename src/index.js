a736a5099be2ed25d76e9af9a47bf3129afd000e
Mehmet Ali Peker <23459375+MrPeker@users.noreply.github.com>
2023-02-11 00:07:50 +0300

    fix cors error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-var cors = require('cors')
+const cors = require('cors')
 app.use(express.json())
 
 const { router } = require('./routes')
 
-app.use('/api', router)
 app.use(cors())
+
+app.use('/api', router)
 
 app.listen(process.env.PORT, () => console.log('App started'))
