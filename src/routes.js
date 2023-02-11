67a53edb707b33e2a2b4f51cada1929f2906eca9
Furkan <furkan_aslan2209@hotmail.com>
2023-02-11 22:52:27 +0300

    types için endpointler ve queryler oluşturuldu

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
-const { getAllLocations, insertLocation, updateLocation } = require('./db/')
+const { getAllLocations, insertLocation, updateLocation, deleteLocation, getAllTypes, insertType, updateType, deleteType } = require('./db/')
 const types = require('../data/types.json')
 const subtypes = require('../data/subtypes.json')
 const cityData = require('../data/city-data.json')
-const { deleteLocation } = require('./db/queries')
 
 // get all locations
 router.get('/', async (req, res) => {
@@ -127,6 +126,79 @@ router.delete('/location/:id', async (req, res) => {
     return res.json({ ok: false, msg: 'Bir hata oluştu', code: 500 }).status(500)
   }
 })
+
+router.get('/types', async (req, res) => {
+  try{
+    let types = getAllTypes();
+    return res.json({
+      ok: true,
+      data: types
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({ok: false, msg: 'Bir hata oluştu', code: 500}).status(500);
+  }
+});
+
+router.post('/types', async (req, res) => {
+  try{
+    if (req.headers['authorization'] !== process.env.SEED_KEY) {
+      return res.json({ ok: false, msg: 'Yetkiniz yok', code: 401 }).status(401)
+    }
+
+    let { type } = req.body;
+
+    const keys = Object.keys(type);
+    const requiredKeys = ['name'];
+    const missingKeys = requiredKeys.filter((key) => !keys.includes(key));
+    if (missingKeys.length) {
+      return res.json({
+        ok: false,
+        message: `Eksik anahtarlar: ${missingKeys.join(', ')}`,
+        code: 400,
+      }).status(400)
+    }
+
+    await insertType(type);
+    return res.json({ok: true});
+  } catch (e) {
+    console.log(e);
+    return res.json({ok: false, msg: 'Bir hata oluştu', code: 500}).status(500);
+  }
+});
+
+router.post('/types/:id', async (req, res) => {
+  try{
+    if (req.headers['authorization'] !== process.env.SEED_KEY) {
+      return res.json({ ok: false, msg: 'Yetkiniz yok', code: 401 }).status(401)
+    }
+
+    let { id } = req.params;
+    let { type } = req.body;
+
+    await updateType(id, type);
+    return res.json({ok: true });
+  } catch (e) {
+    console.log(e);
+    return res.json({ok: false, msg: 'Bir hata oluştu', code: 500}).status(500);
+  }
+});
+
+router.delete('/types/:id', async (req, res) => {
+  try {
+    if (req.headers['authorization'] !== process.env.SEED_KEY) {
+      return res.json({ ok: false, msg: 'Yetkiniz yok', code: 401 }).status(401)
+    }
+
+    let { id } = req.params;
+
+    await deleteType(id);
+    return res.json({ok: true });
+   } catch (e) {
+    console.log(e);
+    return res.json({ok: false, msg: 'Bir hata oluştu', code: 500}).status(500);
+  }
+});
 
 router.get('/health', (req, res) => {
   res.status(200).send()
